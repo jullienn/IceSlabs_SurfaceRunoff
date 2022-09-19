36adbe5791dd99bf064afff4baf2374a67872c5b
JULLIEN Nicolas <nicolas.jullien@unifr.ch>
2022-09-19 19:04:38 +0200

    Get rud of things not usefull anymore. Work with the pickle dataset where elevation and region is already stored for each ice slab point. Do not look for ice slabs within a certain radius around the location of Ys, but rather within a buffer around the elevation of Ys within the polygon of interest.

--- Runoff_VS_IceThickness.py
+++ Runoff_VS_IceThickness.py
@@ -38,20 +38,6 @@ crs = ccrs.NorthPolarStereo(central_longitude=-45., true_scale_latitude=70.)
 crs_proj4 = crs.proj4_init
 ###################### From Tedstone et al., 2022 #####################
 
-'''
-#Define path 2010 2018 data
-path_2010_2018='C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2002_2018/final_excel/high_estimate/'
-#Load all 2010-2018 data
-df_2010_2018_csv = pd.read_csv(path_2010_2018+'Ice_Layer_Output_Thicknesses_2010_2018_jullienetal2021_high_estimate.csv',delimiter=',',decimal='.')
-#Transform the coordinated from WGS84 to EPSG:3413
-transformer = Transformer.from_crs("EPSG:4326", "EPSG:3413", always_xy=True)
-points=transformer.transform(np.asarray(df_2010_2018_csv["lon"]),np.asarray(df_2010_2018_csv["lat"]))
-#Store lat/lon in 3413
-df_2010_2018_csv['lon_3413']=points[0]
-df_2010_2018_csv['lat_3413']=points[1]
-#Extract year and create a new column
-df_2010_2018_csv['year']=[int(df_2010_2018_csv.iloc[x]['Track_name'][0:4]) for x in range(len(df_2010_2018_csv))]
-'''
 ### ---------------------------- Load dataset ---------------------------- ###
 #Dictionnaries have already been created, load them
 path_df_with_elevation='C:/Users/jullienn/switchdrive/Private/research/RT1/final_dataset_2002_2018/' 
@@ -71,21 +57,8 @@ Emax_TedMach=pd.read_csv(path_data+'rlim_annual_maxm/xytpd.csv',delimiter=',',de
 Emax_TedMach=Emax_TedMach.rename(columns={"index":"index_Emax"})
 #Emax_TedMach.drop(columns=['Unnamed: 0'])
 
-'''
-#Extract year, this is from https://stackoverflow.com/questions/41918115/how-do-i-extract-the-date-year-month-from-pandas-dataframe
-Emax_TedMach['date']=pd.to_datetime(Emax_TedMach['date'])
-Emax_TedMach['year'] = Emax_TedMach['date'].dt.year
-'''
 Emax_plus_Mad_TedMach=pd.read_csv(path_data+'rlim_annual_maxm/xytpd_plus_mad.csv',delimiter=',',decimal='.')
-'''
-Emax_plus_Mad_TedMach['date']=pd.to_datetime(Emax_plus_Mad_TedMach['date'])
-Emax_plus_Mad_TedMach['year'] = Emax_plus_Mad_TedMach['date'].dt.year
-'''
 Emax_Ted_minus_Mad_Mach=pd.read_csv(path_data+'rlim_annual_maxm/xytpd_minus_mad.csv',delimiter=',',decimal='.')
-'''
-Emax_Ted_minus_Mad_Mach['date']=pd.to_datetime(Emax_Ted_minus_Mad_Mach['date'])
-Emax_Ted_minus_Mad_Mach['year'] = Emax_Ted_minus_Mad_Mach['date'].dt.year
-'''
 
 #Plot to check
 fig = plt.figure(figsize=(10,5))
@@ -181,23 +154,11 @@ for indiv_index in flowlines_polygones.index:
         #Define Ys point
         Ys_point=np.transpose(np.asarray([np.asarray(within_points_Ys[within_points_Ys.year==indiv_year]['X']),np.asarray(within_points_Ys[within_points_Ys.year==indiv_year]['Y'])]))   
         
-        '''
-        #Define the elevation vector as full of nan
-        subset_iceslabs_retained['elevation']=np.nan
-        
-        #Elevation extraction is from paper Greenland Ice Sheet Ice Slab Expansion and Thickening, function 'extract_elevation.py'
-        for i in range(0,subset_iceslabs_retained.size):
-            #This is from https://gis.stackexchange.com/questions/190423/getting-pixel-values-at-single-point-using-rasterio
-            #Extract elevation in ice slabs dataset
-            for val in GrIS_DEM.sample([(subset_iceslabs_retained['lon_3413'].iloc[i], subset_iceslabs_retained['lat_3413'].iloc[i])]): 
-                #Calculate the corresponding elevation
-                subset_iceslabs_retained['elevation'].iloc[i]=val
-        '''
         #Extract elevation of Ys
         for val in GrIS_DEM.sample(Ys_point): 
             #Calculate the corresponding elevation
             Ys_point_elevation=val[0]
-                
+        
         #Keep only data where elevation is within elevation+/-buffer
         subset_iceslabs_retained_buffered=subset_iceslabs_retained[np.logical_and(subset_iceslabs_retained['elevation']<=(Ys_point_elevation+buffer),subset_iceslabs_retained['elevation']>=(Ys_point_elevation-buffer))]
         
@@ -210,24 +171,6 @@ for indiv_index in flowlines_polygones.index:
         plt.show()
         pdb.set_trace()
         
-        '''
-        #OR EXTRACT ELEVATION OF Ys, AND EXCLUDE ALL SLABS WHOSE ELEVATION IS LARGER THAN THIS ELEVATION +/ A WINDOW
-
-        #For this particular year, extract the distribution of ice slabs at the Emax and Ys location        
-        #This is from https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.BallTree.html
-        SlabsLocTree=np.transpose(np.asarray([np.asarray(subset_iceslabs_retained['lon_3413']),np.asarray(subset_iceslabs_retained['lat_3413'])]))
-        tree = BallTree(SlabsLocTree, leaf_size=2)  
-        # indices of neighbors within distance r
-        ind = tree.query_radius(Ys_point, r=5000)         
-        #Display the ice slabs points that are inside this radius
-        ax2.scatter(subset_iceslabs_retained.iloc[ind[0]]['lon_3413'],subset_iceslabs_retained.iloc[ind[0]]['lat_3413'],color='green',s=10)
-        
-        #Display the slab thickness distribution
-        ax3.hist(subset_iceslabs_retained.iloc[ind[0]]['20m_ice_content_m'])
-        #continue
-        plt.show()
-        pdb.set_trace()
-        '''
         #if vari > than x, add a label in df_2010_2018_csv to indicate not to use it, or just discard it
         # - do yearly maps!
     
